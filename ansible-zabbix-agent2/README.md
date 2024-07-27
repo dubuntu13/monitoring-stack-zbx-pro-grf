4c2aa079cc452e2357f94e5e1a593687b538410e
dani <skyline@skyline>
2024-07-27 14:02:00 +0330

    add-ansible-za2

---
+++ ansible-zabbix-agent2/README.md
@@ -0,0 +1,3 @@
+# ansible-zabbix-agent
+This is a complete ansible playbook for installing zabbix-agent2 on multible server with multiple distributions.
+First task I creat as "create directory" for copy the deb/rpm to it, this is optional, because my situation was that my servers has no access to internet for download, so that force me to copy it localy.
---
+++ ansible-zabbix-agent2/roles/zabbix-agent2/files/support/Kuber-Master-cleanup.sh
@@ -0,0 +1,33 @@
+#!/bin/bash
+
+[[ `whoami` != 'root' ]] && echo "Permission denied please run with sudo command" && exit 1
+
+echo "Cleaning up general logs"
+
+find /var/log/ -iname "messages*" -type f -size +10M -exec rm -rf {} + && \
+    echo "/var/logs/messages logs removed"
+
+find /run/log/journal -type f -size +10M -exec rm -rf {} +  && \
+    echo "/run/log/journal logs removed"
+
+find /var/lib/docker -iname "*.log" -size +1G -delete && \
+    echo "/var/lib/docker logs removed"
+
+find /var/log/journal -type f -iname system@* -delete
+
+find /var/log/journal -type f -iname  system.journal -exec truncate -s 0 {} \;
+
+find /var/log/journal -type f -iname  user@  -delete
+
+find /var/log/journal -type f -iname user-*.journal -exec truncate -s 0 {} \;
+
+###V2
+find /var/log/mongodb -iname "mongo[sd].log" -type f -size +10M -exec truncate -s 0 {} +  && \
+    echo "/var/log/mongodb logs removed"
+
+find /mnt/ -size +1G -type f \( -iname "mgov.log" -o -iname "samp.log" -o -iname "vap.log" -o -iname "my.log" -o -iname "myiran.log" -o -iname "aaa.log" -o -iname "sampadmin.log" -o -iname "sampuser.log" \) -exec truncate -s 0 {} + && \
+#    echo "app logs removed."
+
+###
+
+for item in `kubectl get pod -A --no-headers |grep -v Running | awk '{print $1"|"$2}'`; do kubectl delete pod -n ${item%|*} ${item#*|} --grace-period=5; done
